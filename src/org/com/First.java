bf79beb1a692f428f9e140f4805f8855499ae9a1
kk <karkuzhaliomrbranch@gmail.com>
2025-04-18 12:47:14 +0530

    Second Commit

--- src/org/com/First.java
+++ src/org/com/First.java
@@ -1,5 +1,18 @@
 package org.com;
 
 public class First {
+	
+	public void m1() {
+		System.out.println("Welcome");
+	}
+	
+	public void m2() {
+		System.out.println("Greens Technologies");
+	}
+	public static void main(String[] args) {
+		
+		First task = new First();
+		task.m1();
+	}
 
 }
